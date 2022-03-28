eeee95deb678ccd1ec513fe01c4cd918f6c73dd5
chusloj <chusloj@gmail.com>
2022-03-27 21:36:29 -0600

    new function to ensure surface normal faces against incoming ray

--- hittable.hpp
+++ hittable.hpp
@@ -7,6 +7,13 @@ struct hit_record {
 	point3 p;
 	vec3 normal;
 	double t;
+	bool front_face;
+
+	// front-facing means that the ray and it's normal point do not face against each other geometrically
+	inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+		front_face = dot(r.direction, outward_normal) < 0; // note: 1 line function
+		normal = front_face ? outward_normal : -outward_normal; // note: "normal" is defined in hit_record
+	}
 };
 
 class hittable {
